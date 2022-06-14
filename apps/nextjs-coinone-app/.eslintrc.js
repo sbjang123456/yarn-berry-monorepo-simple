b90a7ceb2a90f75b6cbf5563dee7e00ed55daf5a
byeol <byeol4001@gmail.com>
2022-06-14 11:01:14 +0900

    feat: add new project(nextjs-coinone-app) in apps (not settings)

---
+++ apps/nextjs-coinone-app/.eslintrc.js
@@ -0,0 +1,54 @@
+/**
+ * Specific eslint rules for this app/package, extends the base rules
+ * @see https://github.com/belgattitude/nextjs-monorepo-example/blob/main/docs/about-linters.md
+ */
+
+const { getDefaultIgnorePatterns } = require('@sb/eslint-config-bases/helpers');
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+  },
+  ignorePatterns: [...getDefaultIgnorePatterns(), '.next', '.out'],
+  extends: [
+    '@sb/eslint-config-bases/typescript',
+    '@sb/eslint-config-bases/regexp',
+    '@sb/eslint-config-bases/jest',
+    '@sb/eslint-config-bases/react',
+    '@sb/eslint-config-bases/rtl',
+    // Add specific rules for nextjs
+    'plugin:@next/next/core-web-vitals',
+    // Apply prettier and disable incompatible rules
+    '@sb/eslint-config-bases/prettier',
+  ],
+  rules: {
+    // https://github.com/vercel/next.js/discussions/16832
+    '@next/next/no-img-element': 'off',
+    // For the sake of example
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md
+    'jsx-a11y/anchor-is-valid': 'off',
+  },
+  overrides: [
+    {
+      files: ['src/pages/\\_*.{ts,tsx}'],
+      rules: {
+        'react/display-name': 'off',
+      },
+    },
+    {
+      files: ['src/backend/**/*graphql*schema*.ts'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            // Fine-tune naming convention for graphql resolvers and allow PascalCase
+            selector: ['objectLiteralProperty'],
+            format: ['camelCase', 'PascalCase'],
+          },
+        ],
+      },
+    },
+  ],
+};
