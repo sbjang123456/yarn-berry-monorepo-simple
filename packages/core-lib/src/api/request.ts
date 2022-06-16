e0394431d5c33d611c65fae37e4f4655f2763e7e
JangSuBin <sbjang123456@gmail.com>
2022-06-16 13:33:22 +0900

    chore: add CustomAxiosInstance interface (unification of response format)

--- packages/core-lib/src/api/request.ts
+++ packages/core-lib/src/api/request.ts
@@ -1,20 +1,34 @@
+import type {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosInterceptorManager,
+  AxiosResponse,
+} from 'axios';
 import axios from 'axios';
 
-declare module 'axios' {
-  export interface AxiosInstance {
-    request<T>(config: AxiosRequestConfig): Promise<T>;
-    get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
-    delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
-    head<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
-    post<T>(url: string, data?: T, config?: AxiosRequestConfig): Promise<T>;
-    put<T>(url: string, data?: T, config?: AxiosRequestConfig): Promise<T>;
-    patch<T>(url: string, data?: T, config?: AxiosRequestConfig): Promise<T>;
-  }
+interface CustomAxiosInstance extends AxiosInstance {
+  interceptors: {
+    request: AxiosInterceptorManager<AxiosRequestConfig>;
+    response: AxiosInterceptorManager<AxiosResponse>;
+  };
+  getUri(config?: AxiosRequestConfig): string;
+  request<T>(config: AxiosRequestConfig): Promise<T>;
+  get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  head<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  options<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  post<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
+  put<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
+  patch<T>(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig
+  ): Promise<T>;
 }
 
 export const HOST_COINPAPRIKA_API = 'https://api.coinpaprika.com/v1';
 
-const request = axios.create({
+const request: CustomAxiosInstance = axios.create({
   timeout: 10000,
   headers: { 'content-type': 'application/json' },
 });
