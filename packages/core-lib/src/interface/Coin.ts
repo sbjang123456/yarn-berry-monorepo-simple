23178d5cc464ad8bfd7e7b4ef0862d3bf5d3eddc
hanbyeol <byeol4001@gmail.com>
2022-06-20 14:17:52 +0900

    feat: add ICoin type

--- packages/core-lib/src/interface/Coin.ts
+++ packages/core-lib/src/interface/Coin.ts
@@ -1,14 +1,33 @@
 export interface ICoin {
-  rank: number;
+  beta_value: number;
+  circulating_supply: number;
+  first_data_at: string;
+  id: string;
+  last_updated: string;
+  max_supply: number;
   name: string;
+  rank: number;
   symbol: string;
+  total_supply: number;
   quotes: {
     KRW: {
-      price: number;
+      ath_date: string;
+      ath_price: number;
       market_cap: number;
-      volume_24h: number;
-      percent_change_24h: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
       percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
     };
   };
 }
