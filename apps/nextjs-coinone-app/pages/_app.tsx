346e5236d1a59e8c9e9746c645b353eca8be0dcc
hanbyeol <byeol4001@gmail.com>
2022-06-21 12:28:39 +0900

    refactor: add imported code comment

--- apps/nextjs-coinone-app/pages/_app.tsx
+++ apps/nextjs-coinone-app/pages/_app.tsx
@@ -1,10 +1,17 @@
-import '../styles/globals.css';
-import 'antd/dist/antd.css';
+/* next, react */
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+
+/* components */
 import { Header } from '@/components/Header';
+
+/* lib */
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { useState } from 'react';
+
+/* style */
+import '../styles/globals.css';
+import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
