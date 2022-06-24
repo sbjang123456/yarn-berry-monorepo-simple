f642e53435a08f61e549438d023b0851d85fa36f
hanbyeol <byeol4001@gmail.com>
2022-06-24 10:38:38 +0900

    refactor: separating form
    하위 컴포넌트에서 form item 사용하고, 접근하도록 변경
    상위 컴포넌트에서 form 내용 취합 할 수 있도록 함

--- apps/nextjs-coinone-app/src/containers/Register/index.tsx
+++ apps/nextjs-coinone-app/src/containers/Register/index.tsx
@@ -4,13 +4,39 @@ import { FC } from 'react';
 /* components */
 import Register01 from './Register01';
 
+/* lib */
+import { Form } from 'antd';
+
 /* style */
-import { RegisterContentWrap } from './Register.style';
+import { cssRegisterTitle, RegisterContentWrap } from './Register.style';
 
 const Index: FC = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <RegisterContentWrap>
-      <Register01 />
+      <div css={cssRegisterTitle}>
+        본인인증을 시작합니다. <br />
+        예상 소요시간은 10분입니다.
+      </div>
+      <Form
+        form={form}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Register01 />
+      </Form>
     </RegisterContentWrap>
   );
 };
