60b7f1744880cafe3d0a938eafac53529f4dff13
hanbyeol <byeol4001@gmail.com>
2022-06-29 15:47:02 +0900

    feat: add register data to recoil

--- apps/nextjs-coinone-app/src/containers/Register/index.tsx
+++ apps/nextjs-coinone-app/src/containers/Register/index.tsx
@@ -5,25 +5,39 @@ import type { ValidateErrorEntity } from 'rc-field-form/lib/interface';
 
 /* next */
 import type { FC } from 'react';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { registerStep, registerDataObj } from 'states';
 
-/* style */
+/* components , style */
 import { cssRegisterTitle, RegisterContentWrap } from './Register.style';
-
-/* components */
 import Register01 from './Register01';
+import Register02 from './Register02';
 
 const Index: FC = () => {
   const [form] = Form.useForm();
   const [title, setTitle] = useState('');
+  const [step, setStep] = useRecoilState(registerStep);
+  const [registerData, setRegisterData] = useRecoilState(registerDataObj);
 
   const onFinish = (values: FormData) => {
-    console.log('Success:', values);
+    setStep(step + 1);
+    setRegisterData({ ...registerData, ...values });
+    console.log('Success:', { ...registerData, ...values });
   };
 
   const onFinishFailed = (errorInfo: ValidateErrorEntity) => {
     console.log('Failed:', errorInfo);
   };
+
+  const registerComponents = useMemo(() => {
+    switch (step) {
+      case 1:
+        return <Register01 setTitle={(value: string) => setTitle(value)} />;
+      case 2:
+        return <Register02 setTitle={(value: string) => setTitle(value)} />;
+    }
+  }, [step]);
 
   return (
     <RegisterContentWrap>
@@ -38,7 +52,7 @@ const Index: FC = () => {
         onFinishFailed={onFinishFailed}
         autoComplete="off"
       >
-        <Register01 setTitle={(value: string) => setTitle(value)} />
+        {registerComponents}
       </Form>
     </RegisterContentWrap>
   );
