5c9f2ed44c3bd7e1f6eed6f904fc2f291aa5fdd9
hanbyeol <byeol4001@gmail.com>
2022-07-06 17:09:59 +0900

    chore: component-lib > add storybook note addon, add note

--- packages/component-lib/src/stories/Button.stories.tsx
+++ packages/component-lib/src/stories/Button.stories.tsx
@@ -8,6 +8,9 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  parameters: {
+    notes: 'some documentation here',
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args: ButtonProps) => (
@@ -17,28 +20,43 @@ const Template: ComponentStory<typeof Button> = (args: ButtonProps) => (
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Button1',
+};
+Primary.parameters = {
+  notes: 'Primary notes text',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: 'Button2',
+};
+Secondary.parameters = {
+  notes: 'Secondary notes text',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Button',
+  label: 'Button3',
+};
+Large.parameters = {
+  notes: 'Large notes text',
 };
 
 export const Medium = Template.bind({});
 Medium.args = {
   size: 'medium',
-  label: 'Button',
+  label: 'Button4',
+};
+Medium.parameters = {
+  notes: 'Medium notes text',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'Button',
+  label: 'Button5',
+};
+Small.parameters = {
+  notes: 'Small notes text',
 };
